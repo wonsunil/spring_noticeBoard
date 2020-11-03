5a6729931784cfbb9f728ccbf32543a88f36bcbd
wonsunil <seonil6352@gmail.com>
2020-11-03 17:07:19 +0900

    Add email duplicate check jsp file mapping

--- src/main/java/sunil/noticeBoard/controller/UserController.java
+++ src/main/java/sunil/noticeBoard/controller/UserController.java
@@ -2,6 +2,7 @@ package sunil.noticeBoard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -9,15 +10,25 @@ public class UserController {
     @GetMapping("/user/account/login")
     public String goToLoginPage() {
         return "user/account/login";
-    }
+    };
 
     @GetMapping("/user/account/register")
     public String goToRegisterPage() {
         return "user/account/register";
-    }
+    };
 
     @GetMapping("/user/profile")
     public String goToProfilePage() {
         return "user/profile";
-    }
+    };
+
+    @GetMapping("/user/account/duplicate_check")
+    public String goToDuplicateCheck() {
+        return "user/account/duplicateCheck";
+    };
+
+    @PostMapping("/user/account/duplicate_check")
+    public String goToDuplicateCheck2() {
+        return "user/account/duplicateCheck";
+    };
 }
