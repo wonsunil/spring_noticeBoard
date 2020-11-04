77ee60f4bbae6713b9f2fcd311b726461cf680ef
wonsunil <seonil6352@gmail.com>
2020-11-04 17:35:57 +0900

    Add post mapping for send data to main page

--- src/main/java/sunil/noticeBoard/controller/MainController.java
+++ src/main/java/sunil/noticeBoard/controller/MainController.java
@@ -3,21 +3,25 @@ package sunil.noticeBoard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String redirectMainPage() {
-        return "redirect:http://localhost:8090/main";
+        return "redirect:/main";
     }
 
     @GetMapping("/main")
-    public String goToMainPage(Model model, String userEmail) {
+    public String goToMainPageGet() {
+        return "main";
+    };
+
+    @PostMapping("/main")
+    public void goToMainPagePost(Model model, String userEmail) {
         if(userEmail != null) {
             model.addAttribute("userEmail", userEmail);
         };
-
-        return "main";
-    }
+    };
 }
