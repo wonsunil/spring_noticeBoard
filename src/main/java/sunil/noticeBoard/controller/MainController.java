c709223efd7afac3a26297a6c350c9b3dd666641
wonsunil <seonil6352@gmail.com>
2020-11-06 18:13:44 +0900

    Add model attribute for login user data

--- src/main/java/sunil/noticeBoard/controller/MainController.java
+++ src/main/java/sunil/noticeBoard/controller/MainController.java
@@ -1,11 +1,17 @@
 package sunil.noticeBoard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import sunil.noticeBoard.DataList;
+
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 @Controller
 public class MainController {
+    DataList dataList = new DataList();
 
     @GetMapping("/")
     public String redirectMainPage() {
@@ -13,7 +19,21 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String goToMainPageGet() {
+    public String goToMainPageGet(Model model, HttpSession session) throws SQLException {
+        String email = (String) session.getAttribute("email");
+        String sql = "select writer from content where writer='"+email+"'";
+        String sql2 = "select id from comments where id='"+email+"'";
+        String sql3 = "select id from likes where id='"+email+"'";
+
+        String[] contentWriterList = dataList.getList(sql, new String[0]);
+        model.addAttribute("contentWriterList", contentWriterList);
+
+        String[] commentWriterList = dataList.getList(sql2, new String[0]);
+        model.addAttribute("commentWriterList", commentWriterList);
+
+        String[] likeContentsList = dataList.getList(sql3, new String[0]);
+        model.addAttribute("likeContentsList", likeContentsList);
+
         return "main";
     };
 
