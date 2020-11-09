24ff72a94199dccd06885b255300c8abe5fde176
wonsunil <seonil6352@gmail.com>
2020-11-09 17:24:30 +0900

    Add: data source configuration file for mybatis

--- src/main/java/sunil/noticeBoard/config/DataSourceConfig.java
+++ src/main/java/sunil/noticeBoard/config/DataSourceConfig.java
@@ -1,2 +1,16 @@
-package sunil.noticeBoard.config;public class DataSourceConfig {
-}
+package sunil.noticeBoard.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    };
+};
