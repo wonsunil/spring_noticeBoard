c9fda6e38d1ea422a2df194968e795ebf3a08e1c
wonsunil <seonil6352@gmail.com>
2020-11-09 17:28:54 +0900

    Add: java class file which use implement service file

--- src/main/java/sunil/noticeBoard/service/MemberServiceImpl.java
+++ src/main/java/sunil/noticeBoard/service/MemberServiceImpl.java
@@ -1,2 +1,20 @@
-package sunil.noticeBoard.service;public interface MemberServiceImpl {
+package sunil.noticeBoard.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import sunil.noticeBoard.mapper.MemberMapper;
+import sunil.noticeBoard.model.Member;
+
+import java.util.List;
+
+public class MemberServiceImpl implements MemberService {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Override
+    public List<Member> getAllMember() {
+        List<Member> member = (List<Member>) memberMapper.getAllMember();
+
+        return member;
+    }
 }
