0efba7e5853ec7e867a8717bb763e228af43c92d
wonsunil <seonil6352@gmail.com>
2020-11-09 17:27:17 +0900

    Add: mybatis mapper file

--- src/main/java/sunil/noticeBoard/mapper/MemberMapper.java
+++ src/main/java/sunil/noticeBoard/mapper/MemberMapper.java
@@ -1,2 +1,13 @@
-package sunil.noticeBoard.mapper;public interface MemberMapper {
-}
+package sunil.noticeBoard.mapper;
+
+import sunil.noticeBoard.model.Member;
+
+import java.util.List;
+
+public interface MemberMapper {
+    Member selectMemberByEmail(String Email);
+
+    List<Member> getAllMember();
+
+    void insertMemberData(Member member);
+};
