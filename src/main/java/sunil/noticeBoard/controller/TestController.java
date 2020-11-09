846f04cb9456dc87d9264adb095a454e561c21a2
wonsunil <seonil6352@gmail.com>
2020-11-09 17:26:52 +0900

    Add: test controller for mybatis

--- src/main/java/sunil/noticeBoard/controller/TestController.java
+++ src/main/java/sunil/noticeBoard/controller/TestController.java
@@ -1,5 +1,6 @@
 package sunil.noticeBoard.controller;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import sunil.noticeBoard.model.Member;
 import sunil.noticeBoard.service.MemberService;
 
 @RestController
+@MapperScan(basePackages = "sunil.noticeBoard.mapper")
 public class TestController {
     @Autowired
     MemberService memberService;
