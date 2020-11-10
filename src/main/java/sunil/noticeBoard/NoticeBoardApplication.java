259aa05ee1b6cd2a47e5a8fbd4b7a3a1c09a1c1f
wonsunil <seonil6352@gmail.com>
2020-11-10 16:34:51 +0900

    Add: mapper scan annotation

--- src/main/java/sunil/noticeBoard/NoticeBoardApplication.java
+++ src/main/java/sunil/noticeBoard/NoticeBoardApplication.java
@@ -1,12 +1,13 @@
 package sunil.noticeBoard;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("sunil.noticeBoard.mapper")
 public class NoticeBoardApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NoticeBoardApplication.class, args);
 	};
-}
+};
