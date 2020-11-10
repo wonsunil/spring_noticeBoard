f50b1e1e37aa4302bd4922fd1d4ab17cf9155022
wonsunil <seonil6352@gmail.com>
2020-11-10 17:34:05 +0900

    Refactor: add mybatis service

--- src/main/java/sunil/noticeBoard/controller/MainController.java
+++ src/main/java/sunil/noticeBoard/controller/MainController.java
@@ -1,29 +1,43 @@
 package sunil.noticeBoard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sunil.noticeBoard.DataList;
+import sunil.noticeBoard.model.User;
+import sunil.noticeBoard.service.UserService;
 
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    UserService userService;
+
     DataList dataList = new DataList();
 
     @GetMapping("/")
     public String redirectMainPage() {
         return "redirect:/main";
-    }
+    };
 
     @GetMapping("/main")
     public String goToMainPageGet(Model model, HttpSession session) throws SQLException {
         String email = (String) session.getAttribute("email");
         String sql = "SELECT WRITER FROM CONTENT_INFO WHERE WRITER='"+email+"'";
-        String sql2 = "SELECT ID FROM COMMENTS WHERE ID='"+email+"'";
-        String sql3 = "select ID FROM LIKES WHERE ID='"+email+"'";
+        String sql2 = "SELECT EMAIL FROM COMMENTS WHERE EMAIL='"+email+"'";
+        String sql3 = "select EMAIL FROM LIKES WHERE EMAIL='"+email+"'";
+
+        List<User> userColumnDataList = userService.getUserColumnByCondition("WRITER", "WRITER", email);
+        User[] userColumnDataArray = userColumnDataList.toArray(new User[userColumnDataList.size()]);
+
+        System.out.print(userColumnDataArray);
+        System.out.print(Arrays.toString(userColumnDataArray));
 
         String[] contentWriterList = dataList.getList(sql, new String[0]);
         model.addAttribute("contentWriterList", contentWriterList);
