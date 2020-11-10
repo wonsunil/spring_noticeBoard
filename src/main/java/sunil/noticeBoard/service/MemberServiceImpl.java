e4d27b22fb7200d5670ef9e558bc4ad10d641988
wonsunil <seonil6352@gmail.com>
2020-11-10 16:37:15 +0900

    Refactor: remove unnecessary data type change method

--- src/main/java/sunil/noticeBoard/service/MemberServiceImpl.java
+++ src/main/java/sunil/noticeBoard/service/MemberServiceImpl.java
@@ -1,19 +1,21 @@
 package sunil.noticeBoard.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import sunil.noticeBoard.mapper.MemberMapper;
 import sunil.noticeBoard.model.Member;
 
 import java.util.List;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 
     @Autowired
-    private MemberMapper memberMapper;
+    private MemberMapper mapper;
 
     @Override
     public List<Member> getAllMember() {
-        List<Member> member = (List<Member>) memberMapper.getAllMember();
+        List<Member> member = mapper.getAllMember();
 
         return member;
     }
