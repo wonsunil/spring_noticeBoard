c9ecb8b9eb763fae2f6e982728656d92da17dee6
wonsunil <seonil6352@gmail.com>
2020-11-10 17:35:48 +0900

    Add: rest controller for get all user list

--- src/main/java/sunil/noticeBoard/controller/RestUserController.java
+++ src/main/java/sunil/noticeBoard/controller/RestUserController.java
@@ -1,2 +1,22 @@
-package sunil.noticeBoard.controller;public class RestUserController {
-}
+package sunil.noticeBoard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sunil.noticeBoard.model.User;
+import sunil.noticeBoard.service.UserService;
+
+import java.util.List;
+
+@RestController
+public class RestUserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getAllUser")
+    public List<User> getAllUser() {
+        List<User> userList = userService.getAllUser();
+
+        return userList;
+    };
+};
