4264f129f8ed6540ac285d12c75caacbbb34e522
wonsunil <seonil6352@gmail.com>
2020-11-12 17:22:43 +0900

    Add: load content url mapping

--- src/main/java/sunil/noticeBoard/controller/RestContentController.java
+++ src/main/java/sunil/noticeBoard/controller/RestContentController.java
@@ -1,2 +1,24 @@
-package sunil.noticeBoard.controller;public class RestContentController {
-}
+package sunil.noticeBoard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sunil.noticeBoard.model.Content;
+import sunil.noticeBoard.service.ContentService;
+
+import java.util.List;
+
+@RestController
+public class RestContentController {
+    @Autowired
+    ContentService contentService;
+
+    @GetMapping("/content/user-content")
+    public Content[] userContent(@RequestParam String email) {
+        List<Content> contentList = contentService.getContentByEmail(email);
+        Content[] contents = contentList.toArray(new Content[0]);
+
+        return contents;
+    };
+};
