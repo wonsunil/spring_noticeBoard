c3a5b5d0a933b78db0a7ac554d4a876f790e54d3
wonsunil <seonil6352@gmail.com>
2020-11-12 17:17:57 +0900

    Refacotr: something

--- src/main/java/sunil/noticeBoard/controller/ContentController.java
+++ src/main/java/sunil/noticeBoard/controller/ContentController.java
@@ -1,2 +1,17 @@
-package sunil.noticeBoard.controller;public class ContentController {
-}
+package sunil.noticeBoard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import sunil.noticeBoard.service.ContentService;
+
+@Controller
+public class ContentController {
+    @Autowired
+    ContentService contentService;
+
+    @GetMapping("/content/content-write")
+    public String contentWrite() {
+        return "/content/content_write";
+    };
+};
