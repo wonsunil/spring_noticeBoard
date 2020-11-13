33c65e89c356c071c6648fcb1f2057b8a37bbcf4
wonsunil <seonil6352@gmail.com>
2020-11-13 15:48:40 +0900

    Nothing to do

--- src/main/java/sunil/noticeBoard/service/FollowService.java
+++ src/main/java/sunil/noticeBoard/service/FollowService.java
@@ -1,10 +1,12 @@
 package sunil.noticeBoard.service;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
 import sunil.noticeBoard.model.Follow;
 
 import java.util.List;
 
+@Service
 public interface FollowService {
     List<Follow> getFollower(@Param("email") String email);
     List<Follow> getFollowing(@Param("email") String email);
--- src/main/java/sunil/noticeBoard/service/FollowServiceImpl.java
+++ src/main/java/sunil/noticeBoard/service/FollowServiceImpl.java
@@ -1,11 +1,13 @@
 package sunil.noticeBoard.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import sunil.noticeBoard.mapper.FollowMapper;
 import sunil.noticeBoard.model.Follow;
 
 import java.util.List;
 
+@Service
 public class FollowServiceImpl implements FollowService {
     @Autowired
     FollowMapper mapper;
