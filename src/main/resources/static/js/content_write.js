f4411f95c5582479a36f87b33684c51ad6395774
wonsunil <seonil6352@gmail.com>
2020-11-13 15:46:29 +0900

    Feat: content write method and styling

---
+++ src/main/resources/static/js/content_write.js
@@ -0,0 +1,67 @@
+window.onload = function() {
+    function getFormatDate(date){
+        const year = date.getFullYear();
+
+        let month = (1 + date.getMonth());
+        month = month >= 10 ? month : '0' + month;
+
+        let day = date.getDate();
+        day = day >= 10 ? day : '0' + day;
+
+        return  year + "/" + month + "/" + day;
+    }
+
+    const file_type_check = img => img.match(new RegExp(/\.png|\.jpg|\.jpeg|\.git|\.bmp/));
+
+    const $form = document.forms[0];
+    const $board = $form.children[0];
+    const $writer = $form.children[1];
+    const $contentName = $form.children[2];
+    const $content = $form.children[4];
+
+    $form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const form = new FormData();
+
+        if($content.innerHTML === null) {
+            $content.focus();
+
+            return alert("공백은 넣을 수 없습니다.");
+        };
+
+        form.append("boardName", $board.innerHTML);
+        form.append("writer", $writer.innerHTML.replaceAll(" ", "").split(":")[1]);
+        form.append("contentName", $contentName.value);
+        form.append("content", $content.value);
+        form.append("date", getFormatDate(new Date()));
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "/content/content-write");
+        xhr.send(form);
+
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4) {
+                if(xhr.response) location.href = "/main";
+            };
+        };
+    });
+
+    const $img = document.querySelector("input[type='file']");
+    $img.addEventListener("change", function({ target }) {
+        if(file_type_check(target.value) === null) {
+            target.value = "";
+            return alert("이미지 파일만 선택가능합니다");
+        };
+
+        return;
+    });
+
+    const $selectBox = document.querySelector("select");
+    $selectBox.addEventListener("change", function({ target }) {
+        const value = [...target.children].filter( v => v.selected)[0].innerHTML;
+
+        document.querySelector("#boardName").innerHTML  = value;
+    });
+};
--- src/main/webapp/WEB-INF/content/content_write.jsp
+++ src/main/webapp/WEB-INF/content/content_write.jsp
@@ -1,10 +1,32 @@
+<%@ page import="java.util.Date" %>
+<%@ page import="java.text.SimpleDateFormat" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
+
+<%
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+%>
 
 <html>
 <head>
     <title>Title</title>
+    <link rel="stylesheet" href="/css/content_write.css">
+    <script src="/js/content_write.js"></script>
 </head>
 <body>
-
+    <select name="" id="">
+        <option value="free">자유 게시판</option>
+        <option value="life">일상 게시판</option>
+        <option value="report">신고 게시판</option>
+        <option value="inquire">문의 게시판</option>
+    </select>
+    <form action="/content/content-write" method="POST" enctype="multipart/form-data">
+        <li id="boardName" name="boardName">자유 게시판</li>
+        <li id="writer" name="writer">작성자 : <%= session.getAttribute("email") %></li>
+        <input id="title" name="contentName" placeholder="제목">
+        <il id="date" name="date">작성일 : <%= format.format(new Date())%></il>
+        <textarea name="" id="content" name="content" cols="30" rows="10"></textarea>
+        <li>파일 첨부 : </li><input type="file">
+        <input type="submit" value="글쓰기">
+    </form>
 </body>
 </html>
