4c0829b3ecad565a74ee8a0c1610f079a10c7c54
wonsunil <seonil6352@gmail.com>
2020-11-17 17:16:59 +0900

    Feat: user search

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,22 @@
 window.onload = function() {
+    const xhr = new XMLHttpRequest();
 
+    xhr.responseType = "json";
+
+    document.body.addEventListener("keyup", function({ target, key }) {
+        if(target.getAttribute("id") !== "search-content") return false;
+
+        if(key === "Enter") {
+            xhr.open("GET", "/content/search?contentName="+target.value+"");
+            xhr.send();
+
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === 4) {
+                    if(xhr.response.length === 0) return alert("검색결과가 없습니다");
+
+                    return location.href = `/main?search=${target.value}`;
+                };
+            };
+        };
+    });
 };
