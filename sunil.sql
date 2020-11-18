d08a018766f237fd0f573573c55312b117bf2253
wonsunil <46786330+wonsunil@users.noreply.github.com>
2020-11-18 17:48:43 +0900

    Add files via upload

---
+++ sunil.sql
@@ -0,0 +1,13 @@
+SELECT table_name FROM tabs;
+
+SELECT * FROM user_info;
+SELECT * FROM content_INFO;
+SELECT * FROM comments;
+SELECT * FROM likes;
+
+DESC user_info;
+DESC CONTENT_INFO;
+DESC comments;
+DESC likes;
+
+COMMIT;
