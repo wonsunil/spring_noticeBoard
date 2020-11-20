518a2fc32fc2c656196b41432a465c017fb04868
wonsunil <seonil6352@gmail.com>
2020-11-20 13:23:02 +0900

    Refactor: change url path parameter

--- src/main/java/sunil/noticeBoard/controller/ContentController.java
+++ src/main/java/sunil/noticeBoard/controller/ContentController.java
@@ -3,16 +3,24 @@ package sunil.noticeBoard.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import sunil.noticeBoard.model.Content;
 import sunil.noticeBoard.model.Likes;
+import sunil.noticeBoard.model.User;
 import sunil.noticeBoard.service.ContentService;
 import sunil.noticeBoard.service.LikesService;
+import sunil.noticeBoard.service.UserService;
 
 import java.util.List;
 
 @Controller
 public class ContentController {
+    @Autowired
+    UserService userService;
+
     @Autowired
     ContentService contentService;
 
@@ -49,7 +57,7 @@ public class ContentController {
     };
 
     @GetMapping("/content/{contentCode}")
-    public String showContentDetail(Model model, @PathVariable String contentCode) {
+    public String showContentDetail(Model model, @PathVariable("contentCode") String contentCode) {
         List<Content> contentList = contentService.getContentByContentCode(contentCode);
         Content[] content = contentList.toArray(new Content[0]);
 
@@ -60,10 +68,23 @@ public class ContentController {
 
     @GetMapping("/content/{contentCode}/likes")
     public String likes(Model model, @PathVariable("contentCode") String contentCode) {
+        List<Content> contentList = contentService.getContentByContentCode(contentCode);
+        Content[] content = contentList.toArray(new Content[0]);
+
         List<Likes> likesList = likesService.getLikes(contentCode);
         Likes[] likes = likesList.toArray(new Likes[0]);
 
+        User[] users = new User[likes.length];
+
+        for(int i = 0, length = likes.length; i < length; i++) {
+            List<User> userList = userService.getUserByEmail(likes[i].toArray()[0]);
+
+            users[i] = (User) userList.toArray()[0];
+        }
+
+        model.addAttribute("content", content[0].toArray());
         model.addAttribute("likes", likes);
+        model.addAttribute("users", users);
 
         return "/content/detail";
     };
