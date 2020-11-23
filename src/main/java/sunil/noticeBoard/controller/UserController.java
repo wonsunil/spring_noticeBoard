f3124010de9f593236e7d9fe7c359521223802c2
wonsunil <seonil6352@gmail.com>
2020-11-23 17:21:08 +0900

    Chore: simply login model

--- src/main/java/sunil/noticeBoard/controller/UserController.java
+++ src/main/java/sunil/noticeBoard/controller/UserController.java
@@ -43,11 +43,7 @@ public class UserController {
 
         if (user.length > 0) {
             try{
-                session.setAttribute("email", user[0].toArray()[0]);
-                session.setAttribute("name", user[0].toArray()[1]);
-                session.setAttribute("phone", user[0].toArray()[2]);
-                session.setAttribute("rank", user[0].toArray()[3]);
-
+                session.setAttribute("user", user[0].toArray());
             } catch(ArrayIndexOutOfBoundsException ignored) {
 
             };
