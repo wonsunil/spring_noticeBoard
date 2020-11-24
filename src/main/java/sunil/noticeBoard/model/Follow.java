bcdce26a1a6c783d94309fbb92a61647d395040f
wonsunil <seonil6352@gmail.com>
2020-11-24 14:31:30 +0900

    Feat: add toArray method

--- src/main/java/sunil/noticeBoard/model/Follow.java
+++ src/main/java/sunil/noticeBoard/model/Follow.java
@@ -10,4 +10,10 @@ public class Follow {
     public String getFollowing() {
         return following;
     };
+
+    public String[] toArray() {
+        String[] followArray = {this.follower, this.following};
+
+        return followArray;
+    };
 };
