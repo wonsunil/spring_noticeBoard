e3bce085df97ca3b28625b8f98f2eedcdbd1aac4
wonsunil <seonil6352@gmail.com>
2020-11-24 14:48:44 +0900

    Feat: add profile image variable

--- src/main/java/sunil/noticeBoard/model/Follow.java
+++ src/main/java/sunil/noticeBoard/model/Follow.java
@@ -3,6 +3,7 @@ package sunil.noticeBoard.model;
 public class Follow {
     private String follower;
     private String following;
+    private String profile_image;
 
     public String getFollower() {
         return follower;
@@ -10,9 +11,22 @@ public class Follow {
     public String getFollowing() {
         return following;
     };
+    public String getProfileImage() {
+        return profile_image;
+    };
+
+    public void setFollower(String follower) {
+        this.follower = follower;
+    };
+    public void setFollowing(String following) {
+        this.following = following;
+    };
+    public void setProfileImage(String profileImage) {
+        this.profile_image = profileImage;
+    };
 
     public String[] toArray() {
-        String[] followArray = {this.follower, this.following};
+        String[] followArray = {this.follower, this.following, this.profile_image};
 
         return followArray;
     };
