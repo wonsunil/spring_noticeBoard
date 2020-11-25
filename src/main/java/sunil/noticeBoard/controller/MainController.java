1b5f47c1a47ef9b16e2165b7ed48e332439be507
wonsunil <seonil6352@gmail.com>
2020-11-25 16:23:14 +0900

    Chore: change code

--- src/main/java/sunil/noticeBoard/controller/MainController.java
+++ src/main/java/sunil/noticeBoard/controller/MainController.java
@@ -50,22 +50,23 @@ public class MainController {
         Content[] allContentArray = allContentList.toArray(new Content[0]);
 
         int allContentsLength = allContentArray.length;
+        int count = allContentsLength / page.getLimit();
 
         page.setRange(allContentsLength);
 
-
-
         if((allContentsLength % 10) != 0) {
-            page.setLastPage((int) (Math.floor(allContentsLength / page.getLimit()) + 1));
+            page.setLastPage(count + 1);
         } else{
-            page.setLastPage((int) Math.floor(allContentsLength / page.getLimit()));
+            page.setLastPage(count);
         };
 
         if(currentPage != null) page.setCurrentPage(Integer.parseInt(currentPage));
 
-        if(page.getCurrentPage() == 1) page.setStartIndex(1);
-        if(page.getCurrentPage() > 1)
+        if (page.getCurrentPage() == 1) {
+            page.setStartIndex(1);
+        } else {
             page.setStartIndex((page.getCurrentPage() - 1) * page.getLimit());
+        };
 
         page.setLastIndex(page.getCurrentPage() * page.getLimit());
 
